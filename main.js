48f8ee49b6706c0bcf6a094bc8eb4dc35ebb1f66
ErickOliveiraS <108140024+ErickOliveiraS@users.noreply.github.com>
2023-02-04 14:15:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.slider-principal').slick({
+dots: false,
+infinte: true,
+speed: 300,
+slidesToShow: 1
+})
+
+$('.slider-principal2').slick({
+      slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 700,
+    })
+    
